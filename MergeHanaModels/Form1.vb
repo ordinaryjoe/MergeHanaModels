6b2d51a55d6abf61fd1dc3fce114e74192950a7a
Blair Krotenko <ordinaryjoe@live.com>
2017-10-17 17:54:17 -0400

    Another attempt to write appended data.

--- MergeHanaModels/Form1.vb
+++ MergeHanaModels/Form1.vb
@@ -29,6 +29,11 @@ Public Class Form1
     End Sub
 
     Private Sub btnMerge_Click(sender As Object, e As EventArgs) Handles btnMerge.Click
+        Dim strDataSource As String
+        Dim strCalcViews As String
+        Dim strLogicalMod As String
+        Dim strLayout As String
+
         ' The XmlDocument that we want to merge from.
         Dim xdFrom As New XmlDocument()
         'xdFrom.Load("C:\XmlTest2.xml")
@@ -43,10 +48,55 @@ Public Class Form1
         ' We don't want to copy the root node in this instance.
         For Each nod As XmlNode In xdFrom.DocumentElement.ChildNodes
             ' Import the node to our "to" document.
-            Dim tmpNod As XmlNode = xdTo.ImportNode(nod, True)
+            Dim tmpNod As XmlNode = xdFrom.ImportNode(nod, True)
             ' Append this temporary node to the end of the "to" document
             ' but inside the root element.
-            xdTo.DocumentElement.AppendChild(tmpNod)
+            'If nod.Name = "Datasources" Then
+            '    strDataSource = nod.InnerXml
+            'End If
+            Select Case nod.Name
+                Case "dataSources"
+                    strDataSource = nod.InnerXml
+                Case "calculationViews"
+                    strCalcViews = nod.InnerXml
+                Case "logicalModel"
+                    strLogicalMod = nod.InnerXml
+                Case "layout"
+                    strLayout = nod.InnerXml
+            End Select
+            'xdTo.DocumentElement.AppendChild(tmpNod)
+        Next nod
+
+        For Each nod As XmlNode In xdTo.DocumentElement.ChildNodes
+            ' Import the node to our "to" document.
+            Dim tmpNod As XmlNode = xdTo.ImportNode(nod, True)
+            Dim doc = New XmlDocument()
+            Dim tmpDoc = New XmlDocument()
+            'Dim newNod As XmlNode = doc.CreateNode(strDataSource, "ds", )
+            ' Append this temporary node to the end of the "to" document
+            ' but inside the root element.
+            'If nod.Name = "Datasources" Then
+            '    strDataSource = nod.InnerXml
+            'End If
+            Select Case nod.Name
+                Case "dataSources"
+                    tmpDoc.LoadXml(strDataSource)
+                    xdTo.DocumentElement.AppendChild(tmpDoc)
+                    'strDataSource = nod.InnerXml
+                Case "calculationViews"
+                    tmpDoc.LoadXml(strCalcViews)
+                    xdTo.DocumentElement.AppendChild(tmpDoc)
+                    'strCalcViews = nod.InnerXml
+                Case "logicalModel"
+                    tmpDoc.LoadXml(strLogicalMod)
+                    xdTo.DocumentElement.AppendChild(tmpDoc)
+                    'strLogicalMod = nod.InnerXml
+                Case "layout"
+                    tmpDoc.LoadXml(strLayout)
+                    xdTo.DocumentElement.AppendChild(tmpDoc)
+                    'strLayout = nod.InnerXml
+            End Select
+            'xdTo.DocumentElement.AppendChild(tmpNod)
         Next nod
 
         ' Save the "to" document with it's newly appended nodes.
